bcfbce08ec1fd06b8d2fc9269f556dcb3bb21432
FabioJunik <fabiojunik@gmail.com>
2022-07-24 15:50:36 +0100

    home da parte da aluno criada

--- src/pages/student/homeStyles.ts
+++ src/pages/student/homeStyles.ts
@@ -1,5 +1,20 @@
 import styled from "styled-components";
-
+import {FaStar} from "react-icons/fa";
+import * as sc from "../components/styledComponents"
 
 export const Container = styled.div``;
 
+export const Content = styled(sc.Content)``;
+
+export const OrderCard = styled(sc.Card)`
+    .midiumCard{
+        justify-content: start;
+    }
+`;
+
+export const StarIcon = styled(FaStar)`
+    margin-top: 1rem;
+    margin-right: 0.5rem;
+    font-size: 1.6rem;
+    color: var(--blue-300);
+`
--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -1,12 +1,104 @@
 import { NextPage } from "next";
 import Header from "../components/Header";
-import { Container } from "./homeStyles";
+import { Content, Title, Button} from "../components/styledComponents";
+import { Container, StarIcon, OrderCard} from "./homeStyles";
 
 
 const HomeStudent:NextPage = ()=>{
     return (
         <Container>
             <Header/>
+            <Content>
+                <Title>Pedidos de aulas enviados</Title>
+                <OrderCard>
+                    <div className='topCard'>
+                        <div className='pic'></div>
+                        <div>
+                            <h2>Fábio Dos Santos</h2>
+                            <h3>Matemática</h3>
+                            <div>
+                                <StarIcon/>
+                                <StarIcon/><StarIcon/>
+                                <StarIcon/><StarIcon/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='midiumCard'>
+                        <p>Modo: Presencial</p> |
+                        <p>Nivel: Intermediario</p>
+                    </div>
+                    <div className='bottomCard'>
+                        <Button color='var(--blue-500)'>Ver mais</Button>
+                        <Button color='var(--red-500)'>Cancelar</Button>
+                    </div>                    
+                </OrderCard>
+                <OrderCard>
+                    <div className='topCard'>
+                        <div className='pic'></div>
+                        <div>
+                            <h2>Fábio Dos Santos</h2>
+                            <h3>Matemática</h3>
+                            <div>
+                                <StarIcon/>
+                                <StarIcon/><StarIcon/>
+                                <StarIcon/><StarIcon/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='midiumCard'>
+                        <p>Modo: Presencial</p> |
+                        <p>Nivel: Intermediario</p>
+                    </div>
+                    <div className='bottomCard'>
+                        <Button color='var(--blue-500)'>Ver mais</Button>
+                        <Button color='var(--red-500)'>Cancelar</Button>
+                    </div>                    
+                </OrderCard>
+                <OrderCard>
+                    <div className='topCard'>
+                        <div className='pic'></div>
+                        <div>
+                            <h2>Fábio Dos Santos</h2>
+                            <h3>Matemática</h3>
+                            <div>
+                                <StarIcon/>
+                                <StarIcon/><StarIcon/>
+                                <StarIcon/><StarIcon/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='midiumCard'>
+                        <p>Modo: Presencial</p> |
+                        <p>Nivel: Intermediario</p>
+                    </div>
+                    <div className='bottomCard'>
+                        <Button color='var(--blue-500)'>Ver mais</Button>
+                        <Button color='var(--red-500)'>Cancelar</Button>
+                    </div>                    
+                </OrderCard>
+                <OrderCard>
+                    <div className='topCard'>
+                        <div className='pic'></div>
+                        <div>
+                            <h2>Fábio Dos Santos</h2>
+                            <h3>Matemática</h3>
+                            <div>
+                                <StarIcon/>
+                                <StarIcon/><StarIcon/>
+                                <StarIcon/><StarIcon/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='midiumCard'>
+                        <p>Modo: Presencial</p> |
+                        <p>Nivel: Intermediario</p>
+                    </div>
+                    <div className='bottomCard'>
+                        <Button color='var(--blue-500)'>Ver mais</Button>
+                        <Button color='var(--red-500)'>Cancelar</Button>
+                    </div>                    
+                </OrderCard>
+            </Content>
         </Container>
     )
 }
