2963da1d7b811f0da3bc86c1231367c7b019f1eb
FabioJunik <fabiojunik@gmail.com>
2022-07-29 14:25:00 +0100

    Editando perfil do aluno

--- src/pages/student/profile/index.tsx
+++ src/pages/student/profile/index.tsx
@@ -6,8 +6,43 @@ import Header from '../../components/Header';
 import {Title} from "../../components/styledComponents";
 import {Container, Content, Card, PicContainer, Input, Button} from './styles';
 
+interface UserProps {
+    name: string;
+    lastname: string;
+    gender: string;
+    email: string;
+    password: string;
+    phone1: string;
+    phone2: string;
+}
 
 const Profile:NextPage = ()=>{
+    const {user}= useUser();
+    
+    const [name, setName] = useState<string>(user.name);
+    const [lastname, setLastname] = useState<string>(user.lastname);
+    const [email, setEmail] = useState<string>(user.email);
+    const [password, setPassword] = useState<string>(user.password);
+    const [gender, setGender] = useState<string>(user.gender || '');
+    const [phone1, setPhone1] = useState<string>(user.phone1 || '');
+    const [phone2, setPhone2] = useState<string>(user.phone2 || '');
+
+    function updateUserData(){
+        const ref = database.ref('users/');
+
+        const userData:UserProps ={
+            name,
+            lastname,
+            gender,
+            email,
+            password,
+            phone1,
+            phone2,
+        }
+
+        ref.child(user.id).update(userData);
+        localStorage.setItem('user', JSON.stringify(userData));
+   }
     return (
         <Container>
             <Header/>
@@ -21,23 +56,41 @@ const Profile:NextPage = ()=>{
                     </Card>
                     <Card>
                         <h2>Contacto</h2>
-                        <Input placeholder='Telefone 1'/>
-                        <Input placeholder='Telefone 2'/>
-                        <Input placeholder='E-mail'/>
-                        <Button>Salvar</Button>
+                        <Input 
+                            placeholder='Telefone 1'
+                            value={phone1}
+                            onChange={e=>setPhone1(e.target.value)}
+                        />
+                        <Input 
+                            placeholder='Telefone 2'
+                            value={phone2}
+                            onChange={e=>setPhone2(e.target.value)}
+                        />
+                        <Input placeholder='E-mail' value={email}
+                        onChange={e=>setEmail(e.target.value)}
+                    />
+                        <Button onClick={updateUserData}>Salvar</Button>
                     </Card>
                 </div>
                 <div className='column2'>
                     <Card>
                         <h2>Informações gerais</h2>
-                        <Input placeholder='Nome'/>
-                        <Input placeholder='Sobrenome'/>                            
-                        <select>
+                        <Input 
+                            placeholder='Nome' 
+                            value={name}
+                            onChange={e=>setName(e.target.value)}
+                        />
+                        <Input 
+                            placeholder='Sobrenome' 
+                            value={lastname}
+                            onChange={e=>setLastname(e.target.value)}
+                        />
+                        <select value={gender} onChange={e=>{setGender(e.target.value)}}>
                             <option value="Masculino">Masculino</option>
                             <option value="Femenino">Femenino</option>
                         </select>
                         <Input placeholder='Data de nascimento'/>
-                        <Button>Salvar</Button>
+                        <Button onClick={updateUserData}>Salvar</Button>
                     </Card>
                     <Card>
                         <h2>Identidade</h2>
@@ -49,7 +102,12 @@ const Profile:NextPage = ()=>{
                 <div className='column3'>
                     <Card>
                         <h2>Senha</h2>
-                        <Input type='password'placeholder='Antiga senha' />
+                        <Input 
+                            type='password'
+                            placeholder='Antiga senha' 
+                            value={password}
+                            onChange={e=>setPassword(e.target.value)}
+                        />
                         <Input placeholder='Nova senha'/>
                         <Input placeholder='Confirmar senha'/>
                         <Button>Salvar</Button>
